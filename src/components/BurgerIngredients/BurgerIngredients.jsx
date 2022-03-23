c611c35aa34de186cb7210439d7dc422e88fe555
Andrey Arkhipov <arhipov11@yandex.ru>
2022-03-23 16:35:26 +0300

    Component names changed,amended styles

--- src/components/BurgerIngredients/BurgerIngredients.jsx
+++ src/components/BurgerIngredients/BurgerIngredients.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React  from "react";
 import styles from "./BurgerIngredients.module.css";
 import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
 import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
-import {Counter } from '@ya.praktikum/react-developer-burger-ui-components';
+import { Counter } from '@ya.praktikum/react-developer-burger-ui-components';
+
+
 
 function BurgerTab() {
   const [current, setCurrent] = React.useState("one");
@@ -21,53 +23,46 @@ function BurgerTab() {
   );
 }
 
-function Card({ data }) {
+function BurgerCard({ card }) {
   return (
-   <li className={styles.card}>
-          <a href="#"><img src={data.image} className={styles.card_image}></img></a>
-          <p className={styles.card_price}><CurrencyIcon type="primary" />{data.price}</p>
-      <p className={styles.card_title}>{data.name}</p>
-     <Counter count={10} size="default"  className={ styles.card_counter}/>
+    <li className={styles.card}>
+      <img src={card.image} className={`${styles.card_image} ml-4 mr-4 mb-2`}></img>
+      <p className={`${styles.card_price} text text_type_digits-default mb-2`}>{card.price }<CurrencyIcon type="primary"/></p>
+      <p className={`${styles.card_title} text text_type_main-default mb-7`}>{card.name}</p>
+     <Counter count={3} size="default"  className={ styles.card_counter}/>
    </li>
     
   )
 }
 
-function BurgerIngredients({data}) {
+function ListIngredients({ data,ingredient }) {
+       return (<article > 
+          <h2 className="text text_type_main-medium">{Object.values(ingredient)[0]}</h2>
+         <ul className={`${styles.list_ingredients} mt-6 ml-4 mr-4 mb-9`}>
+           {data.map((card) => {
+         return card.type === Object.keys(ingredient)[0]&&
+             <BurgerCard card={card} key={card._id} /> 
+          })}
+          </ul>
+       </article>
+       )
+      }
+      
+    
+  
+
+function BurgerIngredients({ data }) {
+  const ingredients = [{ bun: "Булки" }, { sauce: "Соусы" }, { main: "Начинки" }]
   return (
     <section className={`${styles.content} pl-5 pr-5`}>
       <h1 className="text text_type_main-large mt-10 mb-5">Соберите бургер</h1>
       <BurgerTab />
-      <article> 
-      <h2 className="text text_type_main-medium">Булки</h2>
-      <ul className={styles.list_ingredients}>
-        {data.map((el) => {
-         return el.type === "bun"&&
-             <Card data={el} key={el._id} />
-          
-          })}
-       </ul>
-    </article>
-    <article>
-        <h2 className="text text_type_main-medium">Соусы</h2>
-      <ul className={styles.list_ingredients}>
-        {data.map((el) => {
-         return el.type === "sauce"&&
-             <Card data={el} key={el._id} />
-          
-          })}
-       </ul>
-      </article>
-      <article>
-        <h2 className="text text_type_main-medium">Начинки</h2>
-      <ul className={styles.list_ingredients}>
-        {data.map((el) => {
-         return el.type === "main"&&
-             <Card data={el} key={el._id} />
-          
-          })}
-       </ul>
-      </article>
+      <div className={styles.ingredients_container}>
+      {
+        ingredients.map((el,index) => {
+          return <ListIngredients data={data} ingredient={el} key={ index}/>
+        })
+    } </div>
       </section>
   );
 }
