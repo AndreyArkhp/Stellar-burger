5b270b27141337171e1ac3136ba93564300b02db
Andrey Arkhipov <arhipov11@yandex.ru>
2022-03-25 17:21:31 +0300

    Changed the logic of the component BurgerConstructor

--- src/components/BurgerConstructor/BurgerConstructor.jsx
+++ src/components/BurgerConstructor/BurgerConstructor.jsx
@@ -1,64 +1,73 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 import { ConstructorElement } from "@ya.praktikum/react-developer-burger-ui-components";
 import { DragIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import { Button } from "@ya.praktikum/react-developer-burger-ui-components";
-import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 
 import styles from "./BurgerConstructor.module.css";
 
+function Oder({ ingredientsPrice, bunPrice }) {
 
-function Oder({ sum }) {
+  const totalPrice = ingredientsPrice.reduce((sum, price) => {
+   return sum += price;
+  }, bunPrice * 2)
+
   return (
     <div className={`${styles.oder} mt-10 mr-5`}>
-      <span className="text text_type_digits-medium mr-10">{`${sum}`}<CurrencyIcon/></span>
+      <span className="text text_type_digits-medium mr-10">
+        {`${totalPrice}`}
+        <CurrencyIcon />
+      </span>
       <Button type="primary" size="large">
-  Оформить заказ
-</Button>
+        Оформить заказ
+      </Button>
     </div>
-  )
+  );
 }
 
 Oder.propTypes = {
-  sum: PropTypes.number,
-}
+  ingredientsPrice: PropTypes.array,
+  bunPrice:PropTypes.number
+};
 
-
-function LockElement({ position }) {
+function LockElement({ position,bun }) {
   const text = {
     top: "верх",
-    bottom:"низ"
-  }
-  return <div className={`${styles.lockElement} mr-2 pr-3`}>
-        <ConstructorElement
-            type={position}
-            isLocked={true}
-            text={`Краторная булка N-200i (${text[position]})`}
-            price={200}
-          thumbnail={"https://code.s3.yandex.net/react/code/bun-02.png"}
-          />
-      </div>
+    bottom: "низ",
+  };
+  return (
+    <div className={`${styles["lock-element"]} mr-2 ml-8 pr-3`}>
+      <ConstructorElement
+        type={position}
+        isLocked={true}
+        text={`${bun.name} (${text[position]})`}
+        price={bun.price}
+        thumbnail={bun.image}
+      />
+    </div>
+  );
 }
 
 LockElement.propTypes = {
   position: PropTypes.string,
-}
+};
 
-function ListElements({card}) {
+function ListIngredients({ ingredient }) {
   return (
-      card.type !=="bun" &&  <li className={`${styles.listElements__item} mb-4 `}>
-           <DragIcon type="primary" />
-               <ConstructorElement
-                 isLocked={false}
-                 text={card.name}
-                 price={card.price}
-                 thumbnail={card.image}
-               />
-         </li>
-  )
+      <li className={`${styles['list-ingredients__item']} mb-4 `}>
+      <div className={`${styles['list-ingridients__icon']} ml-2`}><DragIcon type="primary" /></div>
+        <ConstructorElement
+          isLocked={false}
+          text={ingredient.name}
+          price={ingredient.price}
+          thumbnail={ingredient.image}
+        />
+      </li>
+  );
 }
 
-ListElements.propTypes = {
+ListIngredients.propTypes = {
   card: PropTypes.shape({
     _id: PropTypes.string,
     name: PropTypes.string,
@@ -72,30 +81,37 @@ ListElements.propTypes = {
     image_mobile: PropTypes.string,
     image_large: PropTypes.string,
     __v: PropTypes.number,
-  })
-}
+  }),
+};
 
 function BurgerConstructor({ data }) {
-  let sum = 0;
+  const ingredients = data.filter(element => {
+    return element.type !== "bun"; 
+  })
+
+  const buns = data.filter(bun => {
+    return bun.type === "bun";
+  })
+
+  const ingredientsPrice = [];
 
   return (
     <section className={`${styles.section} mt-25 pr-4 pl-4 `}>
-      <LockElement position={"top"}/>
-       <ul className={`${styles.listElements} mt-4  pr-3`}>
-        {data.map((card) => {
-
-          sum += card.price;
-        return <ListElements card={card} key={card._id} />
-    })}
+      <LockElement position={"top"} bun={ buns[0]}/>
+      <ul className={`${styles['list-ingredients']} mt-4  pr-3`}>
+        {ingredients.map((ingredient) => {
+          ingredientsPrice.push(ingredient.price);
+          return <ListIngredients ingredient={ingredient} key={ingredient._id} />;
+        })}
       </ul>
-      <LockElement position={"bottom"} />
-      <Oder sum={sum}/>
+      <LockElement position={"bottom"} bun={buns[0]}/>
+      <Oder ingredientsPrice={ingredientsPrice} bunPrice={buns[0].price}/>
     </section>
   );
 }
 
 BurgerConstructor.propTypes = {
-  data:PropTypes.arrayOf(PropTypes.object)
-}
+  data: PropTypes.arrayOf(PropTypes.object),
+};
 
 export default BurgerConstructor;
