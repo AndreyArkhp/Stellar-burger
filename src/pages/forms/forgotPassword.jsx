ddd3ba39364b31deb3f5bc2b886554ff55220cbf
Andrey Arkhipov <arhipov11@yandex.ru>
2022-05-17 17:59:57 +0300

    Create request for reset password

--- src/pages/forms/forgotPassword.jsx
+++ src/pages/forms/forgotPassword.jsx
@@ -1,20 +1,49 @@
-import {useState, useRef} from "react";
+import {useState, useRef, useEffect, useCallback} from "react";
 import {Button, Input} from "@ya.praktikum/react-developer-burger-ui-components";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 import styles from "./registrationForms.module.css";
 import Form from "../../components/Form/Form";
+import {baseUrl} from "../../utils/constants";
 
 export default function ForgotPassword() {
   const [value, setValue] = useState("");
   const [error, setError] = useState(false);
+  const [btnDisabled, setBtnDisabled] = useState(true);
   const inputRef = useRef(null);
+  const navigate = useNavigate();
 
-  function handleEmail(e) {
-    const value = e.target.value;
+  const checkEmail = useCallback(() => {
+    return value && /^\w+@[a-z]+\.[a-z]+$/.test(value) ? true : false;
+  }, [value]);
 
-    value ? setError(!/^\w+@\w+\.\w{2,}/.test(value)) : setError(false);
+  function showErorEmail() {
+    console.log(checkEmail());
+    !value ? setError(false) : setError(!checkEmail());
   }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(value);
+    fetch(`${baseUrl}password-reset`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify({email: value}),
+    }).then((res) => {
+      if (res.ok) {
+        navigate("/resetpassword");
+      } else {
+        const error = new Error("Ошибка, попробуйте еще раз");
+        throw error;
+      }
+    });
+  }
+
+  useEffect(() => {
+    checkEmail() ? setBtnDisabled(false) : setBtnDisabled(true);
+  }, [value, checkEmail]);
 
   return (
     <main className={styles.main}>
@@ -24,13 +53,14 @@ export default function ForgotPassword() {
           onChange={(e) => setValue(e.target.value)}
           type={"email"}
           value={value}
+          onBlur={showErorEmail}
+          onFocus={() => setError(false)}
           placeholder={"Укажите e-mail"}
-          onBlur={handleEmail}
           ref={inputRef}
           errorText={"Некоректный email"}
           error={error}
         />
-        <Button type="primary" size="medium">
+        <Button type="primary" size="medium" onClick={handleSubmit} disabled={btnDisabled}>
           Восстановить
         </Button>
       </Form>
